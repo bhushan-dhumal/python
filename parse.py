71883a303ddcc5d4cb4283759ee6e9c675b7cf52
bhushan <bhushan.dhumal07@gmail.com>
2021-08-21 22:34:54 +0530

    refactored code

--- parse.py
+++ parse.py
@@ -13,26 +13,22 @@ data_file = os.path.join(script_directory, file_name)
 
 try:
     f = open(data_file,encoding="utf8")
+    with f:
+        lines = f.readlines();
+        for line in lines:
+            if line[1:9] == today :
+                name = line.split("]")[1].split(":")[0]
+                if name in d :
+                    d[name]+=1
+                else:
+                    d[name] = 1
+                
+    print(dict(sorted(d.items(), key=lambda item: item[1], reverse = True)))
+
+    ## Try to delete the file ##
+    os.remove(data_file)
 except OSError as e:
     print ("Error: %s - %s." % (e.filename, e.strerror))
     sys.exit()
-
-with f:
-    lines = f.readlines();
-    for line in lines:
-        if line[1:9] == today :
-            name = line.split("]")[1].split(":")[0]
-            if name in d :
-                d[name]+=1
-            else:
-                d[name] = 1
-                
-print(dict(sorted(d.items(), key=lambda item: item[1], reverse = True)))
-
-## Try to delete the file ##
-try:
-    os.remove(data_file)
-except OSError as e:  ## if failed, report it back to the user ##
-    print ("Error: %s - %s." % (e.filename, e.strerror))
 
 
